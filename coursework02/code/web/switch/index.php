34f2f6c6c460c978d84853ae281667b7dc4df6b0
Cameron McCallion <1702519@uad.ac.uk>
2019-10-25 22:39:16 +0100

    Added Comments

--- coursework02/code/web/switch/index.php
+++ coursework02/code/web/switch/index.php
@@ -1,8 +1,10 @@
 <?php
+// If read POST request is sent
     if(isset($_POST['read'])) {
         if(isset($_POST['pin'])) {
-            $pin = trim($_POST['pin']);
+            $pin = trim($_POST['pin']); // Trim excess whitespace at the beginning or end
             if(is_numeric($pin) && $pin >= 0 && $pin <= 30) {
+                // Pin is a number & between 0 & 30 (inclusive)
                 echo "Valid\n";
                 echo shell_exec("sudo /home/pi/usertest read " . $_POST['pin']);
             } else {
@@ -12,12 +14,12 @@
             echo "No pin specified";
         }
         die();
-    } else if(isset($_POST['write'])) {
+    } else if(isset($_POST['write'])) { // If write POST request is sent
         if(isset($_POST['pin']) && isset($_POST['value'])){
-            $pin = trim($_POST['pin']);
+            $pin = trim($_POST['pin']); // Get pin & value & trim excess whitespace
             $value = trim($_POST['value']);
-            if($value == "0" || $value == "1"){
-                if(is_numeric($pin) && $pin >= 0 && $pin <= 30){
+            if($value == "0" || $value == "1"){ // If valid value (0/1)
+                if(is_numeric($pin) && $pin >= 0 && $pin <= 30){ // If pin is number and between 0 - 30
                     echo "Valid\n";
                     echo shell_exec("sudo /home/pi/usertest write " . $_POST['pin'] . " " . $_POST['value']);
                 } else {
@@ -29,7 +31,7 @@
         } else {
             echo "No Pin/Value specified";
         }
-        die();
+        die(); // Stop execution.
     }
 ?>
 <!DOCTYPE html>
@@ -99,11 +101,17 @@
             light = false,
             read, write, timeout;
 
+        /**
+            * Toggle the correct image to show
+         */
         function show_light() {
             light_on.style.display = light ? "block" : "none";
             light_off.style.display = light ? "none" : "block";
         }
-
+        
+        /**
+            * Flip the current state of the LED
+         */
         function toggle_light(e) {
             if(pin && pin.value && pin.value !== "" && !isNaN(pin.value)) {
                 light = !light;
@@ -113,20 +121,23 @@
                 alert("Please enter a valid GPIO Pin Value");
             }
         }
-
+        
+        /**
+            * Send a POST request to read the current value of the pin
+         */
         function read_value(pno) {
             if(read && read.abort) read.abort();
             read = new XMLHttpRequest();
             read.onreadystatechange = function() {
                 if(read.readyState == 4) {
-                    if(read.status == 200) {
+                    if(read.status == 200) { // If the server response is 200 OK
                         toggle_loading(false);
                         let resp = read.responseText;
-                        if(resp.indexOf("val:") > -1) {
-                            let value = resp[resp.indexOf("val:") + 4];
+                        if(resp.indexOf("val:") > -1) { // If the server responds with a pin value
+                            let value = resp[resp.indexOf("val:") + 4]; // Get the pin value
                             if(value == 0 || value == 1) {
-                                light = value == 1;
-                                show_light();
+                                light = value == 1; // Set the pin value (true if 1, false if 0)
+                                show_light(); // Show the correct light
                             } else {
                                 console.log("Unexpected Value", value);
                             }
@@ -140,20 +151,24 @@
             read.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             read.send("read&pin=" + pno);
         }
+        
 
+        /**
+            * Send a POST request to write the value of the current pin
+         */
         function write_value(pno, val) {
             if(write && write.abort) write.abort();
             write = new XMLHttpRequest();
             write.onreadystatechange = function() {
                 if(write.readyState == 4) {
-                    if(write.status == 200) {
+                    if(write.status == 200) { // If the server response is 200 OK
                         toggle_loading(false);
                         let resp = write.responseText;
-                        if(resp.indexOf("val:") > -1) {
-                            let value = resp[resp.indexOf("val:") + 4];
+                        if(resp.indexOf("val:") > -1) { // If the server respinds with a pin value
+                            let value = resp[resp.indexOf("val:") + 4]; // Get the pin value
                             if(value == 0 || value == 1) {
-                                light = value == 1;
-                                show_light();
+                                light = value == 1; // Set the pin value
+                                show_light(); // Show the correct light
                             } else {
                                 console.log("Unexpected Value", value);
                             }
@@ -170,20 +185,33 @@
             write.send("write&pin=" + pno + " &value=" + val)
         }
 
+        /**
+            * Show a loading message
+         */
         function toggle_loading(state) {
             loading.style.display = state ? "block" : "none";
         }
 
+        // Set listener for clicking light
         light_on.addEventListener("click", toggle_light, true);
         light_off.addEventListener("click", toggle_light, false);
 
+        /**
+            * Listen for keystrokes on pin input
+            * After 400ms of no input, send a read POST request
+            * Updates the image to the corrent one based on the pin value
+         */
         pin.addEventListener("keydown", function(e) {
-            console.log("KEYDOWN - Starting timeout");
+            //console.log("KEYDOWN - Starting timeout");
+            // If a timeout is currently waiting to trigger, clear it.
             clearTimeout(timeout);
+            // Set a timeout for 400ms, so as to allow the user time to type the pin
+            // Stops a request being sent for every keystroke
             timeout = setTimeout(function() {
+                // If the user has actually entered a "valid" pin
                 if(pin && pin.value && pin.value !== "" && !isNaN(pin.value)){
                     toggle_loading(true);
-                    console.log("KEYDOWN - Reached timeout");
+                    // console.log("KEYDOWN - Reached timeout");
                     read_value(pin.value);
                 }
             }, 400);
